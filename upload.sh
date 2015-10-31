add3d5c2086594993f4ebc4c9db5dcbebbcc6924
Andrew McGregor <me@andrewmcgregor.me>
2015-10-31 14:49:37 -0400

    stuff

--- upload.sh
+++ upload.sh
@@ -2,4 +2,4 @@ cd _site
 zip -r site.zip .
 scp site.zip andrew@45.79.140.147:
 ssh andrew@45.79.140.147
-# sudo cp site.zip /usr/share/nginx/html/site.zip && rm site.zip && cd /usr/share/nginx/html/ && sudo unzip -o site.zip && sudo rm site.zip && exit
+# sudo cp site.zip /srv/www/polynomicsgame.com/public_html/site.zip && rm site.zip && cd /srv/www/polynomicsgame.com/public_html/ && sudo unzip -o site.zip && sudo rm site.zip && exit
